368c4b052683399b27c37342ae5345dd365db268
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-10-30 11:36:13 +0800

    Add files via upload

---
+++ itl303/svg/svganimation.php
@@ -0,0 +1,13 @@
+<html>
+<head></head>
+<body>
+   <h1>SVG Animation</h1>
+   <p class="codepen" data-height="689" data-theme-id="dark" data-default-tab="html,result" data-user="zygarde1023" data-slug-hash="ExyEggL" style="height: 689px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;" data-pen-title="LT 6.2">
+  <span>See the Pen <a href="https://codepen.io/zygarde1023/pen/ExyEggL">
+  LT 6.2</a> by zygarde1023 (<a href="https://codepen.io/zygarde1023">@zygarde1023</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://static.codepen.io/assets/embed/ei.js"></script>
+    
+</body>
+</html>
