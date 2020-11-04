c3198bef7f263ab904e6dd033fae3a229ef9048a
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-11-04 10:04:42 +0800

    Update clecss.php

--- itl303/css/clecss.php
+++ itl303/css/clecss.php
@@ -1,91 +1,13 @@
-<!DOCTYPE html>
 <html>
-    <head>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-    <link rel="stylesheet" href="mods.css"> 
-    </head>
-    <body class="body">
-    <div class = "div-header">
-        <div class = "div-logo">
-            <div id = "logoA"></div>
-            <div id = "logoB"></div>
-            <div id = "logoC"></div>
-        </div>
-        <div class="div-tb">
-        <div id = "title">BIG Z</div>
-        </div>
-        <div class="div-ls">Welcome Admin</div>
-    </div>
-        
-    <div class="div-body">
-        <div class="prodA">
-        <h1>Product 1st is Our "Chicharoom Specials"</h1>
-	<u><p>The Chicharooms are mushrooms dried and fried for you.<br/>
-	The Chicharooms are Safely Created And Stored In Our Kitchen.<br/>
-	The Chicharooms are Good For Watching movies,
-	Bonding With Friends and Family, Also Good For Gatherings.<br/>
-	The Chicharooms Can be in Different Flavors Like:</p><br/></u>
-	<ul>
-		<li>Cheese</li>
-		<li>Spicy Cheese</li>
-		<li>Sweet Corn</li>
-		<li>Barbeque</li>
-		<li>Xtreme Labuyo</li>
-		<li>Parmesan</li>
-		<li>Sour Cream</li>
-	<ul><br/>
-	<p>Sizes w/ Prices:</p>
-		<li>| 120php/(250g) | 280php/(500g) | 490php/(1kg)</li>
-        </div>
-        
-        <div class="prodB">
-        <h1 class = "font-weight-bold">Product 2 is Our "Batchoy Z"</h1>
-	<u><p>The Batchoy Z is one of the Best in Philippines.<br/>
-	The Batchoy Z was Pegged as Zinger Batchoy Nationwide.<br/>
-	The Batchoy Z is Good For Time with your Love, Good For Everyone,<br/>
-	Bonding With Friends and Family, Also Good For Gatherings.<br/>
-	The Batchoy Z Can be in Different Styles Like:</p><br/></u>
-	<ul class = "list-ticked">
-		<li>Zinger Batchoy</li>
-		<li>Batchoy HotZ</li>
-		<li>Batchoy LGDZ</li>
-		<li>Batchoy EGGZ</li>
-		<li>Batchoy Xtreme Z</li>
-		<li>Classic Batchoy Z</li>
-		<li>Batchoy BukoZ</li>
-	<ul><br/>
-	<p class = "h2">Sizes w/ Prices:</p>
-	
-	<ul class = "list-ticked">
-		<li>| 160php/(Medium "1pax") | 320php/(Large "2-3pax") | 600php/(5XL "5-6pax")</li>
-	<ul>
-	<br/>
-        </div>
-        
-    </div>
-        
-    <div class="div-social">
-        <div class="sc">
-            Visit Us @: 
-            <i class='fab fa-facebook-square' style='font-size:100px;color:blue'></i>
-            <i class='fab fa-pinterest-square' style='font-size:100px;color:red'></i>
-            <i class='fab fa-instagram-square' style='font-size:100px;color:blueviolet'></i>
-            <i class='fab fa-twitter-square' style='font-size:100px;color:lightskyblue'></i>
-        </div>
-    </div>
+<head></head>
+<body>
+    <h1>CLE CSS</h1>
+    <p class="codepen" data-height="1345" data-theme-id="dark" data-default-tab="html,result" data-user="zygarde1023" data-slug-hash="LYZrJPL" style="height: 1345px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;" data-pen-title="CLE CSS">
+  <span>See the Pen <a href="https://codepen.io/zygarde1023/pen/LYZrJPL">
+  CLE CSS</a> by zygarde1023 (<a href="https://codepen.io/zygarde1023">@zygarde1023</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://static.codepen.io/assets/embed/ei.js"></script>
     
-    <div class="div-footer">
-        <div>
-        Please contact us at:<br/>
-        Messenger: BigZPhil<br/>
-        Phone(Office): 09523317856<br/>
-        Telephone No.: (034) 442-1234<br/>
-        Email Us at: BigZPhilZ@gmail.com<br/>
-        </div>
-    </div>
-        
-    </body>
-    
-    
+</body>
 </html>
