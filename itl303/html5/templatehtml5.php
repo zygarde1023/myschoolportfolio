5a922f43f4181fd2e41c485ade130e437a446a67
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-11-27 17:03:15 +0800

    Create templatehtml5.php

---
+++ itl303/html5/templatehtml5.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+    <title>My School Work</title>
+</head>
+<style>
+    ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  width: 200px;
+  background-color: teal;
+  float: left;
+}
+
+li a {
+  display: inline-block;
+  color: #000;
+  padding: 8px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color on hover */
+li a:hover {
+  background-color: royalblue;
+  color: white;
+}
+
+.con{
+    float: right;
+    width: 80%;
+}
+
+.nv{
+    width: 20%;    
+}
+</style>
+<body>
+    <div><h2>HTML5 Demo</h2></div>
+    <div class="nv" style="border: 5px;">
+        <ul>
+            <li><a href="#">EMPTY</a></li>
+            <li><a href="#">EMPTY</a></li>
+            <li><a href="#">EMPTY</a></li>
+            <li><a href="#">EMPTY</a></li>
+            <li><a href="#">EMPTY</a></li>
+            <li><a href="#">EMPTY</a></li>
+            <li><a href="#">EMPTY</a></li>
+        </ul>
+    </div>
+    <div class="con">
+    <?php include ($content); ?>
+    </div>
+    <div><?php $content; ?></div>
+</body>
+</html>
