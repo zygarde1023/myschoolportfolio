d278fcb8b82d40fb8c2266692567430a62c1ae05
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-11-27 16:45:07 +0800

    Create svgcle.php

---
+++ itl303/svg/svgcle.php
@@ -0,0 +1,13 @@
+<html>
+<head></head>
+<body>
+   <h1>SVG CLE</h1>
+   <p class="codepen" data-height="530" data-theme-id="light" data-default-tab="html,result" data-user="zygarde1023" data-slug-hash="VwjJXMj" style="height: 530px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;" data-pen-title="SVG CLE">
+  <span>See the Pen <a href="https://codepen.io/zygarde1023/pen/VwjJXMj">
+  SVG CLE</a> by zygarde1023 (<a href="https://codepen.io/zygarde1023">@zygarde1023</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://static.codepen.io/assets/embed/ei.js"></script>
+    
+</body>
+</html>
