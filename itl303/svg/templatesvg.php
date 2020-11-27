bf1704055afe75356e75201f455eff65fb89615f
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-11-27 17:07:38 +0800

    Update templatesvg.php

--- itl303/svg/templatesvg.php
+++ itl303/svg/templatesvg.php
@@ -41,10 +41,7 @@ li a:hover {
             <li><a href="index.php?view=svg&demo=svgcom">SVG Combinations</a></li>
             <li><a href="index.php?view=svg&demo=svgtxt">SVG Text Art</a></li>
             <li><a href="index.php?view=svg&demo=svgamt">SVG Animation</a></li>
-            <li><a href="index.php?view=svg&demo=svgcle">SVG CLE</a></li>
-            <li><a href="#">EMPTY</a></li>
-            <li><a href="#">EMPTY</a></li>
-            <li><a href="#">EMPTY</a></li>
+            <li><a href="index.php?view=svg&demo=svgcle">SVG CLE 2</a></li>
         </ul>
     </div>
     <div class="con">
