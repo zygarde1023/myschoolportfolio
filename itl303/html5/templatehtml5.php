b4ec94fce9c2c454c69057fb0edffda353e47a79
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-11-29 00:01:48 +0800

    Update templatehtml5.php

--- itl303/html5/templatehtml5.php
+++ itl303/html5/templatehtml5.php
@@ -38,7 +38,7 @@ li a:hover {
     <div><h2>HTML5 Demo</h2></div>
     <div class="nv" style="border: 5px;">
         <ul>
-            <li><a href="#">EMPTY</a></li>
+            <li><a href="index.php?view=html5&demo=html5com">HTML5 Canvas Combination</a></li>
             <li><a href="#">EMPTY</a></li>
             <li><a href="#">EMPTY</a></li>
             <li><a href="#">EMPTY</a></li>
