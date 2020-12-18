3a1c911a87145c8cea9676dddde08cef728c07b5
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-12-19 02:23:25 +0800

    Create aboutme.php

---
+++ itl303/aboutme.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+</head>
+<style>
+    h2, h3{
+        margin-left: 200px;
+        font-family: cursive;
+        font-style: normal;
+    }
+    h1{
+        margin-left: 550px;
+        font-family: monospace;
+    }
+    .des{
+        background-color: aquamarine;
+    }
+    .ftr{
+        background-color: limegreen;
+    }
+</style>
+<body>
+   <div>
+    <svg style="width: 2000px; height: 100px;">
+    <rect x="0" y="0" rx="0" ry="0" width="1800" height="100" stroke="none" fill="royalblue"/>
+      <text x="400" y="60" font-size="50" fill="black">ZHANDRO BRYANT CAWALING
+       </text>
+  </svg>
+  </div>
+  <div class="des">
+      <h2>These are the Words that Best describes myself</h2>
+      <h3>>> Talented</h3>
+      <h3>>> Skilled</h3>
+      <h3>>> Passionate</h3>
+      <h3>>> Kind Hearted</h3>
+      <h3>>> "Too Nice"</h3>
+      
+      <h3>Most of the time I hear these words when they refer to me</h3>
+  </div>
+  <div class="ftr">
+      <h1>THATS ALL FOLKS!!!</h1>
+  </div>
+</body>
+</html>
