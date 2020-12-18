bb5612dc3b1f6137281fac928df9572b3f3be7ae
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-12-19 02:25:18 +0800

    Update html5combo.php

--- itl303/html5/html5combo.php
+++ itl303/html5/html5combo.php
@@ -1,7 +1,7 @@
 <html>
 <head></head>
 <body>
-   <h1>HTML5 Canvas Combination</h1>
+   <h1>CANVAS Combination</h1>
    <p class="codepen" data-height="745" data-theme-id="dark" data-default-tab="js,result" data-user="zygarde1023" data-slug-hash="XWjWBMZ" style="height: 745px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;" data-pen-title="LT  7.4">
   <span>See the Pen <a href="https://codepen.io/zygarde1023/pen/XWjWBMZ">
   LT  7.4</a> by zygarde1023 (<a href="https://codepen.io/zygarde1023">@zygarde1023</a>)
